79efc09be52b96b36f8eb11af376bd73ddbcdc69
KristinaSorokina <kris.tina.sorokina@yandex.ru>
2023-01-08 18:30:00 +0300

    add api tests and allure report

---
+++ src/main/java/api/ApiResponse.java
@@ -0,0 +1,20 @@
+package api;
+
+public class Response {
+    private int id;
+
+    public Response(int id) {
+        this.id = id;
+    }
+
+    public Response() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
